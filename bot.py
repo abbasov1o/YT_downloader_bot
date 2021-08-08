9e1ce628999e78d10f182d3559ddd71a6752a6d8
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-08 08:52:18 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,5 +128,70 @@ def audio_dl(client, message):
     except exceptions.HTMLParseError:
         message.reply_text("Given URL couldn't be parsed.")
 
+@bot.on_message(filters.command(['mp3']))
+def mp3(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 Mahnı axtarılır...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('Heç nə tapmadım.\n\nOrfoqrafiyanı bir az dəyişməyə çalışın.')
+            return
+    except Exception as e:
+        m.edit(
+            "✖️ Heç nə tapmadım.\n\nOrfoqrafiyanı bir az dəyişməyə çalışın."
+        )
+        print(str(e))
+        return
+    m.edit("⏬ Yükləyirəm.")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🎧 **Başlıq**: [{title[:35]}]({link})\n⏳ **Müddəti**: `{duration}`\n👁‍🗨 **Baxış**: `{views}`'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ XETA')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e) 
 
 app.run()
